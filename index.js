e38201a652e4b351394158d5e7755c9702044aec
mbaradji221 <mbaradji221@outlook.fr>
2019-06-14 18:15:31 +0200

    add button on toolbar

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 import extensionName from "./js/selection";
-let extension;
 
-document.addEventListener('keydown', function(event) {
+import SpinalWindowSelection from "./js/createToolbar";
+
+const TOOLBAR_GROUP_NAME = "spinalcom";
+
+
+const EXTENSION_NAME = "spinal-window-selection";
+
+let extension;
+let visible = false;
+
+// document.addEventListener('keydown', function(event) {
+//   extension = typeof extension === "undefined" ? window.spinal.ForgeViewer
+//     .viewer
+//     .getExtension(extensionName) : extension;
+
+//   if (event.altKey && event.ctrlKey) {
+//     extension.startSelection();
+//   }
+
+// })
+
+// document.addEventListener('keyup', function(event) {
+//   if (typeof extension !== "undefined" && (event.keyCode === 17 || event
+//       .keyCode === 18)) {
+//     extension.disableSelectionMode();
+//   }
+// })
+
+
+const windowSelection = SpinalWindowSelection({
+  name: "spinal-window-selection",
+  icon: "crop_free",
+  label: "window selection",
+  subToolbarName: TOOLBAR_GROUP_NAME
+}, () => {
   extension = typeof extension === "undefined" ? window.spinal.ForgeViewer
     .viewer
     .getExtension(extensionName) : extension;
 
-  if (event.altKey && event.ctrlKey) {
+  if (!visible) {
     extension.startSelection();
-  }
-
-})
-
-document.addEventListener('keyup', function(event) {
-  if (typeof extension !== "undefined" && (event.keyCode === 17 || event
-      .keyCode === 18)) {
+  } else {
     extension.disableSelectionMode();
   }
-})
+  visible = !visible;
+
+});
+
+window.Autodesk.Viewing.theExtensionManager.registerExtension(
+  EXTENSION_NAME,
+  windowSelection
+);
+
+window.spinal.ForgeExtentionManager.addExtention(EXTENSION_NAME);
---
+++ js/createToolbar.js
@@ -0,0 +1,86 @@
+export default function(params, callback) {
+
+  console.log("pamramslksv", params)
+
+  const ViewerToolbarButtonManager = class {
+
+    constructor(viewer, options) {
+      this.viewer = viewer;
+      window.Autodesk.Viewing.Extension.call(this, viewer, options);
+    }
+
+    load() {
+      if (this.viewer.toolbar) {
+        // Toolbar is already available, create the UI
+        this.createUI();
+      } else {
+        // Toolbar hasn't been created yet, wait until we get notification of its creation
+        this.onToolbarCreatedBinded = this.onToolbarCreated.bind(this);
+        this.viewer.addEventListener(av.TOOLBAR_CREATED_EVENT, this
+          .onToolbarCreatedBinded);
+      }
+
+      return true;
+    }
+
+
+    onToolbarCreated() {
+      this.viewer.removeEventListener(av.TOOLBAR_CREATED_EVENT, this
+        .onToolbarCreatedBinded);
+      this.onToolbarCreatedBinded = null;
+      this.createUI();
+    }
+
+    createUI() {
+      // var viewer = this.viewer;
+
+      // Button 1
+      var button = new window.Autodesk.Viewing.UI.Button(params.name);
+      button.onClick = () => {
+        callback();
+      };
+
+      var icon = button.container.firstChild;
+      icon.className =
+        "adsk-button-icon md-icon md-icon-font md-theme-default";
+
+      icon.innerHTML = params.icon;
+
+      if (typeof params.label !== "undefined")
+        button.setToolTip(params.label);
+
+      this.subToolbar = this.viewer.toolbar.getControl(
+        params.subToolbarName
+      );
+
+      if (!this.subToolbar) {
+        console.log("params.subToolbarName", params.subToolbarName);
+        this.subToolbar = new window.Autodesk.Viewing.UI.ControlGroup(
+          params.subToolbarName
+        );
+
+        this.subToolbar.addControl(button);
+
+        this.viewer.toolbar.addControl(this.subToolbar);
+      } else {
+        this.subToolbar.addControl(button);
+
+      }
+    }
+
+
+    unload() {
+      this.viewer.toolbar.removeControl(this.subToolbar);
+      return true;
+    }
+
+    // registerExtension(name, className) {
+    //   window.Autodesk.Viewing.theExtensionManager.registerExtension(
+    //     name,
+    //     className
+    //   );
+    // }
+
+  }
+  return ViewerToolbarButtonManager;
+}
